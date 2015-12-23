ee1d96ebb96e1ea7e48c8873b7dfdf4c7c3d92f5
pujand <dhakalpujan5@gmail.com>
2015-12-23 09:04:06 +0545

    .gitignore updated to ignore nb related files

---
+++ src/hello/helloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author View9-2
+ */
+public class helloWorld {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0; i<=n; i++){
+        System.out.println("Hello World");
+        }
+    }
+}
